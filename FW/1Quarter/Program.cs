fd44d58a0a52a3f6aa9e9569ed173f3a03e44843
IgorRoma <romigole@gmail.com>
2022-07-08 16:55:33 +0300

    IgorRoma

---
+++ FW/1Quarter/Program.cs
@@ -0,0 +1,5 @@
+﻿// Формирует массив из строк определённой длинны не более 3 символов на строку.
+
+//Console.Clear();
+resultArr = [2, -22, 234, 1111, 9, 33];
+var resultArr = existArr.Where(x=>x.Length < 4).ToArray();
